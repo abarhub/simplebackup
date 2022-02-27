0f30932583fff9934ea7db36369938f64a57d846
abarhub <abarhub@gmail.com>
2022-02-27 08:37:02 +0100

    Initial commit

---
+++ src/main/java/org/simplebackup/simplebackup/SimplebackupApplication.java
@@ -0,0 +1,13 @@
+package org.simplebackup.simplebackup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebackupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplebackupApplication.class, args);
+    }
+
+}
